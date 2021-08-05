cf733a7140c91f91701f921f0b19aa142ad1f969
MiuruRajapaksha <miuru@rumex.lk>
2021-08-05 17:56:42 +0530

    fix request uri issue

--- src/main/java/com/javainuse/config/KafkaConfig.java
+++ src/main/java/com/javainuse/config/KafkaConfig.java
@@ -1,10 +1,15 @@
 package com.javainuse.config;
 
 import com.javainuse.model.Message;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -28,5 +33,24 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, Message> kafkaTemplate(){
         return new KafkaTemplate<String, Message>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "b-2.kafkacluster.ts0w7l.c4.kafka.ap-south-1.amazonaws.com:9092");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId1");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
     }
 }
--- src/main/java/com/javainuse/controller/ItemController.java
+++ src/main/java/com/javainuse/controller/ItemController.java
@@ -4,9 +4,11 @@ import com.javainuse.model.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,10 +21,11 @@ public class ItemController {
     KafkaTemplate<String, Message> KafkaJsontemplate;
     String TOPIC_NAME = "AWSKafkaTutorialTopic";
 
-    @PostMapping(value = "/postItem")
-    public String postJsonMessage(@RequestBody Message message){
+    @GetMapping
+    public String postJsonMessage(@RequestParam("page") int page, @RequestParam("size") int size,
+                                  @RequestParam(value = "search", required = false) String searchTerm){
         log.info("Recieved");
-        KafkaJsontemplate.send(TOPIC_NAME,new Message(message.getId(),message.getTitle(),message.getMessage()));
+        KafkaJsontemplate.send(TOPIC_NAME,new Message(page,searchTerm,searchTerm));
         return "Message published successfully";
     }
 }
